6887820713ac94a2d691e69c1c6e2b99c6966e70
tdergouzi <13177763451@163.com>
2024-03-22 17:19:16 +0800

    Feat: impl program deploy on devnet

--- programs/hello_world/src/lib.rs
+++ programs/hello_world/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+// use program::HelloWorld;
 
 declare_id!("5NjgwpGKcNsR5TNWSmCFTjR4PZ6kjZ4YqncqYJZccxFk");
 
@@ -6,10 +7,63 @@ declare_id!("5NjgwpGKcNsR5TNWSmCFTjR4PZ6kjZ4YqncqYJZccxFk");
 pub mod hello_world {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, data: String) -> Result<()> {
+        msg!("{}", data);
+
+        *ctx.accounts.hello_world = HelloWorld {
+            authority: *ctx.accounts.authority.key,
+            data
+        };
+
+        Ok(())
+    }
+
+    pub fn update(ctx: Context<UpdateHelloWorld>, data: String) -> Result<()> {
+        ctx.accounts.hello_world.data = data;
+        msg!("{}", ctx.accounts.hello_world.data);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + HelloWorld::INIT_SPACE,
+        seeds = [b"hello-world"],
+        bump
+    )]
+    pub hello_world: Account<'info, HelloWorld>,
+    #[account(mut)]
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct HelloWorld {
+    pub authority: Pubkey,
+    #[max_len(100)]
+    pub data: String
+}
+
+#[derive(Accounts)]
+pub struct UpdateHelloWorld<'info> {
+    #[account(
+        mut,
+        seeds = [b"hello-world"],
+        bump
+    )]
+    pub hello_world: Account<'info, HelloWorld>,
+    #[account(mut)]
+    pub authority: Signer<'info>
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("You are not authorized to perform this action.")]
+    Unauthorized,
+    #[msg("Cannot get the bump.")]
+    CannotGetBump,
+}
--- tests/hello_world.ts
+++ tests/hello_world.ts
@@ -4,13 +4,46 @@ import { HelloWorld } from "../target/types/hello_world";
 
 describe("hello_world", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  let provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.HelloWorld as Program<HelloWorld>;
 
+  const authority = provider.wallet.publicKey;
+  let [helloWorld] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("hello-world")],
+    program.programId
+  )
+  console.log("===========helloworld", helloWorld.toBase58());
+  console.log("===========programId", program.programId.toBase58())
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.initialize("Hello My Friend!").accounts({
+      helloWorld,
+      authority,
+      systemProgram: anchor.web3.SystemProgram.programId
+    }).rpc();
+
     console.log("Your transaction signature", tx);
+
+    const accountState = await program.account.helloWorld.fetch(helloWorld);
+    console.log("account state: ", accountState);
   });
+
+  it('get hello world!', async () => {
+    const tx = await program.methods.update('Hello My Life!').accounts({
+      helloWorld
+    }).rpc()
+
+    console.log("Your transaction signature", tx);
+
+    const accountState = await program.account.helloWorld.fetch(helloWorld);
+    console.log("account state: ", accountState);
+  })
+
+  it('read account name', async () => {
+    const accountState = await program.account.helloWorld.fetch(helloWorld);
+    console.log("account state: ", accountState);
+  })
 });
